096fb35974752c10c2f0a87a29a81f26a410140a
Jonny Kalambay <jonnyk20@gmail.com>
2017-10-03 00:45:26 +0000

    finished day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          id: 2,
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+    this.onSubmitMessage = this.onSubmitMessage.bind(this)
+  }
+
+
+  onSubmitMessage(newMessage){
+    this.setState({
+      messages: this.state.messages.concat(newMessage)
+    })
+  }
+  
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar user={this.state.currentUser} onSubmitMessage={this.onSubmitMessage}/>
+      </div>
     );
   }
 }
