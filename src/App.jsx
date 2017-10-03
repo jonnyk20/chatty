42192d189004a28b5e1dfbd6a66a87434f00721b
Jonny Kalambay <jonnyk20@gmail.com>
2017-10-03 16:42:25 +0000

    mew messages added to DOM

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: 'Anonymous'}, 
       messages: []
     }
     this.onSubmitMessage = this.onSubmitMessage.bind(this)
+    this.onUserChange = this.onUserChange.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.onNewMessage = this.onNewMessage.bind(this)
   }
 
+  onUserChange(newUsername) {
+    this.setState({
+      currentUser: {name: newUsername}
+    });
+  }
 
   onSubmitMessage(newMessage){
     this.socket.send(JSON.stringify(newMessage))
@@ -19,13 +27,25 @@ class App extends Component {
   
   componentDidMount(){
     this.socket = new WebSocket('ws://localhost:3001');
+    this.socket.onmessage = (event) => {
+      this.onNewMessage(JSON.parse(event.data))
+    }
+  }
+  onNewMessage(newMessage) {
+    this.setState({
+      messages: this.state.messages.concat(newMessage)
+    });
   }
 
   render() {
     return (
       <div>
         <MessageList messages={this.state.messages}/>
-        <ChatBar user={this.state.currentUser} onSubmitMessage={this.onSubmitMessage}/>
+        <ChatBar 
+          user={this.state.currentUser} 
+          onSubmitMessage={this.onSubmitMessage}
+          onUserChange={this.onUserChange}
+        />
       </div>
     );
   }
