a88451bf762bc6088cf112f9b0530e35b0684709
Jonny Kalambay <jonnyk20@gmail.com>
2017-10-04 14:07:10 +0000

    image works

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import ImageDiv from './ImageDiv.jsx';
 
 class Message extends Component {
   render() {
-    this.messageColors = ['purple', 'blue', 'green', 'orange']
+    this.messageColors = ['purple', 'blue', 'green', 'orange'];
+    const imagePath = new RegExp(/https?:\/\/.*\.(?:png|jpg)/, 'ig');
+    const foundImages = this.props.message.content.match(imagePath);
+    let messageImages;
+    let filteredString = this.props.message.content;
+    if (foundImages !== null){
+        messageImages = foundImages.map((match, index) => {
+        return (<ImageDiv key={index} source={match} /> )
+       })
+       filteredString = this.props.message.content.replace(imagePath, '')
+    }
 
     return (
       <div className="message">
         <span className={'message-username user-' + this.messageColors[this.props.message.userCycle]}>{this.props.message.username}</span>
-        <span className="message-content">{this.props.message.content}</span>
+        <span className="message-content">{filteredString}</span>
+         { foundImages && messageImages}
       </div>
     );
   }
